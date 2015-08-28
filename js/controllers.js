8ee8dacbbafbd5c5c1e9f5d24001fc9a4fb8a28b
BlakeMcS <blacekaleb@gmail.com>
2015-08-28 23:51:19 +1200

    Ordering items and adding controller functions

--- js/controllers.js
+++ js/controllers.js
@@ -1,7 +1,19 @@
-var app = angular.module('menuApp', []);
+var app = angular.module('menuApp', []).filter('object2Array', function() {
+    return function(input) {
+        var out = []; 
+        for(i in input){
+            out.push(input[i]);
+        }
+        return out;
+    }
+});
 
 app.controller('menuCtrl', function($scope, $http) {
   	$http.get('json/menu-items.json').then(function(res){
         $scope.menuItems = res.data;               
     });
+    
+    $scope.isString = function(input) {
+        return typeof input === 'string';
+    };
 });
