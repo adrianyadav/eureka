1d9c952349865d04dfa7105b01bb900256a2ff64
BlakeMcS <blacekaleb@gmail.com>
2015-08-28 15:05:53 +1200

    Tidies up menu css

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,7 @@
 var app = angular.module('menuApp', []);
 
 app.controller('menuCtrl', function($scope, $http) {
-	console.log($scope.firstName);
-	$scope.meme = "my memes are top";
   	$http.get('json/menu-items.json').then(function(res){
-        $scope.menuItems = res.data;     
-    	console.log($scope.menuItems.snacks.fries['gluten-free']);           
+        $scope.menuItems = res.data;               
     });
 });
