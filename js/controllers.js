f376b34059e201c7ecd2b9a1537a4b9630d7852b
BlakeMcS <blacekaleb@gmail.com>
2015-08-31 10:52:23 +1200

    A commit

--- js/controllers.js
+++ js/controllers.js
@@ -9,11 +9,19 @@ var app = angular.module('menuApp', []).filter('object2Array', function() {
 });
 
 app.controller('menuCtrl', function($scope, $http) {
+    $scope.index = 0;
   	$http.get('json/menu-items.json').then(function(res){
-        $scope.menuItems = res.data;               
+        $scope.menuItems = res.data;   
+        $scope.subMenuList = Object.keys($scope.menuItems['snacks']);
+        console.log($scope.subMenuList);
     });
-    
+       
     $scope.isString = function(input) {
         return typeof input === 'string';
     };
+    
+    $scope.isObject = function(input) {
+        return typeof input === 'object';
+        console.log(input);
+    }
 });
