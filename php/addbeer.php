67fcaa62bb1bc704e905b67db87dba76f7762dec
adrianyadav <adrianyadav@gmail.com>
2015-10-08 22:30:20 +1300

    Added security so scripts can't be accessed directly.

--- php/addbeer.php
+++ php/addbeer.php
@@ -1,15 +1,19 @@
 <?php
-
-$drinks = simplexml_load_file('./xml/drinks.xml');
-$beers = $drinks->xpath('//beer');
-$i = 0;
-foreach ($beers as $beer) {
-    $i++;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $drinks = simplexml_load_file('../xml/drinks.xml');
+    $beers = $drinks->xpath('//beer');
+    $i = 0;
+    foreach ($beers as $beer) {
+        $i++;
+    }
+    print($i);
+    $newBeer = $drinks->beers->addChild('beer', $_POST["beer$i"]);
+    $drinks->saveXML('../xml/drinks.xml');
+    header('Location: ../admin.php');
+    exit();
+} else {
+    header('Location: ../admin.php');
+    exit();
 }
-print($i);
-$newBeer = $drinks->beers->addChild('beer', $_POST["beer$i"]);
-$drinks->saveXML('./xml/drinks.xml');
-header('Location: admin.php');
-exit();
 ?>
 
--- php/addhousepour.php
+++ php/addhousepour.php
@@ -1,17 +1,21 @@
 <?php
 session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $drinks = simplexml_load_file('../xml/drinks.xml');
+    $housepours = $drinks->xpath('//housepour');
+    $i = 0;
+    foreach ($housepours as $housepour) {
+        $i++;
+    }
+    print($i);
+    $newHousepour = $drinks->housepours->addChild('housepour', $_POST["housepour$i"]);
+    $drinks->saveXML('../xml/drinks.xml');
+    header('Location: ../admin.php');
+    exit();
 
-
-$drinks = simplexml_load_file('../xml/drinks.xml');
-$housepours= $drinks->xpath('//housepour');
-$i = 0;
-foreach ($housepours as $housepour) {
-    $i++;
+} else {
+    header('Location: ../admin.php');
+    exit();
 }
-print($i);
-$newHousepour = $drinks->housepours->addChild('housepour', $_POST["housepour$i"]);
-$drinks->saveXML('../xml/drinks.xml');
-header('Location: ../admin.php');
-exit();
 ?>
 
