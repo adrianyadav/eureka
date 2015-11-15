42416ce40fc264a3537b6cdf66577ea8c9f8641b
adrianyadav <adrianyadav@gmail.com>
2015-11-16 10:31:33 +1100

    Refactored UpdateXML and DeleteEmptyNodes successfully

--- php/script.php
+++ php/script.php
@@ -1,60 +1,40 @@
 <?php
 session_start();
-?>
 
-<?php
-    function update ($data, $item, $items, $totalitems){
-        $items = $data->xpath("//$item");
-        foreach ($items as $item) {
-            $totalitems++;
-        }
-
-        for ($i = 0; $i < $totalitems; $i++){
-            $data->$items->$item[$i] = $_POST["$item$i"];
-            $handle = fopen("../xml/$items.xml", "wb");
+/*
+* Updates the Drinks XML file with input data
+* @param items, $item
+*
+*/
+function updateDrinksXML ($item, $items){
+    $drink = $item;
+    $drinks = $items;
+    $data = simplexml_load_file("../xml/drinks.xml");
+    $items = $data->xpath("//$drink");
+    $totalitems = 0;
+    foreach ($items as $item) {
+        $totalitems++;
+    }
+    for ($i = 0; $i < $totalitems; $i++){
+        if ($drinks == "beers") {
+            $data->beers->beer[$i] = $_POST["beer$i"];
+            $handle = fopen("../xml/drinks.xml", "wb");
             fwrite($handle, $data->asXML());
             fclose($handle);
-        }   
-
+        } else if ($drinks = "housepours") {
+            $data->housepours->housepour[$i] = $_POST["housepour$i"];
+            $handle = fopen("../xml/drinks.xml", "wb");
+            fwrite($handle, $data->asXML());
+            fclose($handle);
+        }
     }
 
-    function deleteEmptyNodes ($file){
-    // $file  = '../xml/drinks.xml';
-        $xpath = '//*[not(text())]';
-    foreach ($xml->xpath($xpath) as $remove) {
-        unset($remove[0]);
-    }
-        $xml->asXML($file);
-    }
-
-
-?>
-
-<?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $data = simplexml_load_file("../xml/drinks.xml");
-    $beers = $data->xpath('//beer');
-    $totalbeers = 0;
-    foreach ($beers as $beer) {
-        $totalbeers++;
-    }
-    $housepours = $data->xpath('//housepour');
-    $totalhousepours = 0;
-    foreach ($housepours as $housepour) {
-        $totalhousepours++;
-    }
-    for ($i = 0; $i < $totalbeers; $i++){
-        $data->beers->beer[$i] = $_POST["beer$i"];
-        $handle = fopen("../xml/drinks.xml", "wb");
-        fwrite($handle, $data->asXML());
-        fclose($handle);
-    }
-    for ($x = 0; $x < $totalhousepours; $x++){
-        $data->housepours->housepour[$x] = $_POST["housepour$x"];
-        $handle = fopen("../xml/drinks.xml", "wb");
-        fwrite($handle, $data->asXML());
-        fclose($handle);
-    }
+}
+/*
+* Deletes all empty nodes in Drinks.xml
+*
+*/
+function deleteEmptyNodes (){
     $file  = '../xml/drinks.xml';
     $xpath = '//*[not(text())]';
     if (!$xml = simplexml_load_file($file)) {
@@ -64,10 +44,15 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         unset($remove[0]);
     }
     $xml->asXML($file);
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    updateDrinksXML("beer", "beers");
+    updateDrinksXML("housepour", "housepours");
+    deleteEmptyNodes();
     header('Location: ../admin.php');
     exit();
 } else {
-    header('Location:../admin.php');
     exit();
 }
 ?>
